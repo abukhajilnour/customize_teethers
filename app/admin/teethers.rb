fc7aca14b6c510d3224c5548ad23f28bcc91f42b
Nourakh <nour@buckmaster.ca>
2020-11-17 12:42:12 -0800

    add active admin

--- app/admin/teethers.rb
+++ app/admin/teethers.rb
@@ -1,18 +1,12 @@
 ActiveAdmin.register Teether do
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :price, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :keywords, :status, :quantity, :gender, :discount, :kind, :theme, :money, :sale
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :image_file_name, :image_content_type, :image_file_size, :image_updated_at, :keywords, :status, :quantity, :gender, :discount, :kind, :theme, :money, :sale]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+ index do
+  column :name
+  column :money do |p|
+    number_to_currency p.price
+  end
+  column :theme
+
+ end
   
 end
