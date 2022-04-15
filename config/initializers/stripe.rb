f30051a77c9eb7cf58723fa85967878b28d4bea6
abukhajilnour <nour@khatib.ca>
2022-04-15 12:44:29 -0700

    stripe fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,11 @@
 require 'stripe'
 
 
-Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+# Stripe.api_key = Rails.application.credentials[:stripe][:secret]
+
+Rails.configuration.stripe = {
+    :public => ENV['PUBLIC'],
+    :secret => ENV['SECRET']
+
+}
+Stripe.api_key = Rails.configuration.stripe[:secret]
